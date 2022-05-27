f76517a71f9bc0c7c4e995bbc0e65849442d96f1
smoke code <rajeev.jha@gmail.com>
2022-05-28 04:11:59 +0530

    Builder Design Pattern Introduction

---
+++ Builder_Design_Pattern_Introduction/ComputerSystem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Builder_Design_Pattern_Introduction
+{
+    class ComputerSystem
+    {
+        private string _RAM;
+        private string _HDDSize;
+
+        public ComputerSystem()
+        {
+        }
+
+        public ComputerSystem(string RAM, string HDD)
+        {
+            _RAM = RAM;
+            _HDDSize = HDD;
+        }
+
+        public string Build()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append(string.Format(" RAM: {0}", _RAM));
+
+            sb.Append(string.Format(" HDDSize: {0}", _HDDSize));
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Builder_Design_Pattern_Introduction/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Builder_Design_Pattern_Introduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string RAM = "4GB";
+            string HDDSize = "300 GB";
+            ComputerSystem computerSystem = new ComputerSystem(RAM, HDDSize);
+            string SystemConfigurationDetails = computerSystem.Build();
+            Console.WriteLine(SystemConfigurationDetails);
+            Console.ReadLine();
+        }
+    }
+}
