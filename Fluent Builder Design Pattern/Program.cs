a1851b02104669b990f17807a5307b40c90ebd04
smoke code <rajeev.jha@gmail.com>
2022-05-28 10:18:13 +0530

    main

--- Fluent Builder Design Pattern/Program.cs
+++ Fluent Builder Design Pattern/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using Fluent_Builder_Design_Pattern.Builder.ConcreteBuilder;
+using Fluent_Builder_Design_Pattern.Builder.Director;
+using Fluent_Builder_Design_Pattern.Builder.IBuilder;
+using Fluent_Builder_Design_Pattern.Builder.Product;
 
 namespace Fluent_Builder_Design_Pattern
 {
@@ -6,6 +11,18 @@ namespace Fluent_Builder_Design_Pattern
     {
         static void Main(string[] args)
         {
+            Dictionary<string, string> formCollection = new Dictionary<string, string>();
+            formCollection.Add("Drive", "300GB");
+            formCollection.Add("RAM", "4B");
+            formCollection.Add("Mouse", "MTech");
+            formCollection.Add("Keyboard", "KTech");
+            formCollection.Add("TouchScreen", "Yes");
+            //Concrete Builder
+            ISystemBuilder systemBuilder = new LaptopBuilder();
+            //Director
+            ConfigurationBuilder builder = new ConfigurationBuilder();
+            builder.BuildSystem(systemBuilder, formCollection);
+            ComputerSystem system = systemBuilder.GetSystem();
             Console.WriteLine("Hello World!");
         }
     }
